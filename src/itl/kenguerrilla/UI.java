240f4e3cfd64c46603ed9c02b2312e39c23f4af5
Ken Guerrilla <kenguerrilla@gmail.com>
2017-11-08 23:03:37 +0800

    JavaCalcutorDemo beta1.0

--- src/itl/kenguerrilla/UI.java
+++ src/itl/kenguerrilla/UI.java
@@ -11,37 +11,36 @@ import javax.swing.*;
 public class UI extends JFrame implements ActionListener {
 	
 	/**
-	 *  UI排版模組
+	 * 
 	 */
-	
 	private static final long serialVersionUID = 1L;
 	
 	private JMenuBar menuBar;
 	private JMenu aboutMenu;
 	private JMenuItem menuAbout;
+	
+	private String resultBuffer="";
+	private String keyingBuffer="";
+	private String initLabel="0.";
 
 	JFrame frm = new JFrame("Java Calculator Beta01");
 	
-	JPanel number_pnl = new JPanel(new GridLayout(4,3)); // 數字鍵
-	JPanel function_pnl = new JPanel(new GridLayout(5,1)); // 功能鍵
+	JPanel numberPanel = new JPanel(new GridLayout(4,3)); // 數字鍵
+	JPanel functionPanel = new JPanel(new GridLayout(5,1)); // 功能鍵
 	
-	JLabel process_lab = new JLabel("0. ", JLabel.RIGHT); // 運算過程
-	JLabel result_lab = new JLabel("0. ", JLabel.RIGHT); // 運算結果
+	JLabel labProcess = new JLabel(initLabel, JLabel.RIGHT); // 運算過程
+	JLabel labResult = new JLabel(initLabel, JLabel.RIGHT); // 運算結果
 	
-	JButton but[], btn_dot, btn_add, btn_sub, btn_mul, btn_div, btn_clear, btn_equal;
+	JButton but[], btnDot, btnAdd, btnSub, btnMul, btnDiv, btnClear, btnEqual;
 	
-	String numbleKey[] = {"0","1","2","3","4","5","6","7","8","9"};
+	String numberKey[] = {"0","1","2","3","4","5","6","7","8","9"};
 	
-	boolean firstKeying = true;
-	boolean keyingLocked = false;
 	
 	Font font = new Font("細明體",Font.PLAIN,18);
 	
 	Font sFont = new Font("細明體",Font.PLAIN,14);
 	
 	Calculator cal = new Calculator();
-	
-	//double sum =0,sumBuffer =0;
 	
 	
 	public UI(){
@@ -83,10 +82,10 @@ public class UI extends JFrame implements ActionListener {
 		
 		frm.setResizable(false);
 		
-		frm.add(process_lab);
-		frm.add(result_lab);
-		frm.add(number_pnl);
-		frm.add(function_pnl);
+		frm.add(labProcess);
+		frm.add(labResult);
+		frm.add(numberPanel);
+		frm.add(functionPanel);
 		
 		frm.setVisible(true);
 	}
@@ -95,32 +94,25 @@ public class UI extends JFrame implements ActionListener {
 	// init Label
 	private void initLab() {
 		// TODO Auto-generated method stub
-		process_lab.setBounds(20, 40, 200, 30);
-		process_lab.setOpaque(true); // 透明效果
-		process_lab.setBackground(Color.WHITE); // 背景顏色
-		process_lab.setForeground(Color.BLACK); // 前景顏色
-		process_lab.setFont(sFont);
+		labProcess.setBounds(20, 40, 200, 30);
+		labProcess.setOpaque(true); // 透明效果
+		labProcess.setBackground(Color.WHITE); // 背景顏色
+		labProcess.setForeground(Color.BLACK); // 前景顏色
+		labProcess.setFont(sFont);
 		
-		result_lab.setBounds(20, 70, 200, 30);
-		result_lab.setOpaque(true); // 透明效果
-		result_lab.setBackground(Color.WHITE); // 背景顏色
-		result_lab.setForeground(Color.BLACK); // 前景顏色
-		result_lab.setFont(font);
+		labResult.setBounds(20, 70, 200, 30);
+		labResult.setOpaque(true); // 透明效果
+		labResult.setBackground(Color.WHITE); // 背景顏色
+		labResult.setForeground(Color.BLACK); // 前景顏色
+		labResult.setFont(font);
 		
 	}
 	
 	// init Number Button and Function Button
 	private void initKey() {
 		// TODO Auto-generated method stub
-		function_pnl.setBounds(170,120,50,200);
-		number_pnl.setBounds(20, 120, 150, 200);
-		
-		/*
-		for(int i = 1;i <=9; i++){
-			number_pnl.add(new JButton(Integer.toString(i))).setFont(font);
-		}
-		
-		*/
+		functionPanel.setBounds(170,120,50,200);
+		numberPanel.setBounds(20, 120, 150, 200);
 		
 		
 		but = new JButton[10];
@@ -129,44 +121,38 @@ public class UI extends JFrame implements ActionListener {
 			but[i] = new JButton(String.valueOf(i));
 		}
 		
-		
 		for(int i = 1;i < 10; i++){
-			number_pnl.add(but[i]).setFont(font);
+			numberPanel.add(but[i]).setFont(font);
 			but[i].addActionListener(this);
 			
 		}
 		
 		but[0] = new JButton("0");
-		btn_dot = new JButton(".");
-		btn_equal = new JButton("=");
-		btn_add = new JButton("+");
-		btn_sub = new JButton("-");
-		btn_mul = new JButton("*");
-		btn_div = new JButton("/");
-		btn_clear = new JButton("C");
+		btnDot = new JButton(".");
+		btnEqual = new JButton("=");
+		btnAdd = new JButton("+");
+		btnSub = new JButton("-");
+		btnMul = new JButton("*");
+		btnDiv = new JButton("/");
+		btnClear = new JButton("C");
 		
 		but[0].addActionListener(this);
-		btn_dot.addActionListener(this);
-		btn_equal.addActionListener(this);
-		btn_add.addActionListener(this);
-		btn_sub.addActionListener(this);
-		btn_mul.addActionListener(this);
-		btn_div.addActionListener(this);
-		btn_clear.addActionListener(this);
+		btnDot.addActionListener(this);
+		btnEqual.addActionListener(this);
+		btnAdd.addActionListener(this);
+		btnSub.addActionListener(this);
+		btnMul.addActionListener(this);
+		btnDiv.addActionListener(this);
+		btnClear.addActionListener(this);
 		
-		
-		number_pnl.add(btn_dot).setFont(font);
-		number_pnl.add(but[0]).setFont(font);
-		number_pnl.add(btn_equal).setFont(font);
-		function_pnl.add(btn_add).setFont(font);
-		function_pnl.add(btn_sub).setFont(font);
-		function_pnl.add(btn_mul).setFont(font);
-		function_pnl.add(btn_div).setFont(font);
-		function_pnl.add(btn_clear).setFont(font);
-		
-
-		
-
+		numberPanel.add(btnDot).setFont(font);
+		numberPanel.add(but[0]).setFont(font);
+		numberPanel.add(btnEqual).setFont(font);
+		functionPanel.add(btnAdd).setFont(font);
+		functionPanel.add(btnSub).setFont(font);
+		functionPanel.add(btnMul).setFont(font);
+		functionPanel.add(btnDiv).setFont(font);
+		functionPanel.add(btnClear).setFont(font);
 		
 	}
 
@@ -177,92 +163,100 @@ public class UI extends JFrame implements ActionListener {
 		
 		final Object source = e.getSource();
 		
-		if(firstKeying == true){
-			process_lab.setText("");
-			result_lab.setText("");
-			firstKeying = false;
-		}
-		
-		
-		//boolean keyingLocked = false;
 		for(int i=0; i<10;i++){	
 			if(source == but[i]){
-				
-				if(keyingLocked){
-					
-				}
-				process_lab.setText(process_lab.getText() + numbleKey[i]);
-				result_lab.setText(result_lab.getText() + numbleKey[i]);
+				keyingNumber(String.valueOf(i));
+				showBuffer();
 			}
-	
 		}
 		
-		if(source == btn_dot){
+		if(source == btnDot){
 		
+			keyingNumber(".");
+			showBuffer();
+			
 		}
 		
-		if(source == btn_add){
-						
-			process_lab.setText(process_lab.getText() +"+");
+		if(source == btnAdd){
 			
-			writer(cal.checkIn(Calculator.actionMode.Add, reader()));
-			
+			writer(Calculator.actionMode.Add);
+
 		}
 			
-		if(source == btn_sub){
+		if(source == btnSub){
 			
-			process_lab.setText(process_lab.getText() +"-");
-			//cal.functionAct(actionMod.Sub, reader());
+			writer(Calculator.actionMode.Sub);
 
 		}
 		
-		if(source == btn_mul){
-			
-			process_lab.setText(process_lab.getText() +"*");
-			//cal.functionAct(actionMod.Mul, reader());
+		if(source == btnMul){
+
+			writer(Calculator.actionMode.Mul);
 			
 		}
 		
-		if(source == btn_div){
-			
-			process_lab.setText(process_lab.getText() +"/");
-			//cal.functionAct(actionMod.Div, reader());
+		if(source == btnDiv){
+
+			writer(Calculator.actionMode.Div);
 
 		}
 		
-		
-		if(source == btn_clear){
-
-		}
-		
-		if(source == btn_equal){
+		if(source == btnClear){
 			
-			
-			writer(cal.anser(reader()));		
-			
-			cal.show();
+			resetUI();
 			
 		}
+		
+		if(source == btnEqual){
+			
+			writer(Calculator.actionMode.None);
+			
+		}
+		
 	}
 	
-	private Double reader(){
+	private void keyingNumber(String s) {
+		// TODO Auto-generated method stub
 		
-		String str = result_lab.getText();
+		keyingBuffer += s;
+		labResult.setText(keyingBuffer);
 		
-		Double buf = Double.valueOf(str);
+	}
+
+
+	private void writer(Calculator.actionMode mode){
+				
+		resultBuffer = cal.checkIn(keyingBuffer, mode);
+		setLabel();
+		showBuffer();
 		
-		result_lab.setText("");
-		
-		return buf;
 	}
 	
-    public void writer(final Double num) {
-        if (Double.isNaN(num)) {
-            result_lab.setText("");
-        } else {
-        	result_lab.setText(Double.toString(num));
-        }
-}
+	private void setLabel(){
+		
+		keyingBuffer="";
+		labResult.setText(resultBuffer);
+		labProcess.setText(cal.getProcessString());
+	}
+	
+	private void resetUI(){
+		
+		cal.reset();
+		keyingBuffer = "";
+		resultBuffer ="";
+		labProcess.setText(initLabel);
+		labResult.setText(initLabel);
+		showBuffer();
+		
+	}
+    
+    private void showBuffer(){
+    	System.out.println("-----------UI-----------");
+    	System.out.print("resultBuffer:"+resultBuffer);
+    	System.out.println(" keyingBuffer:"+keyingBuffer);
+    	System.out.println("------------------------");
+    	
+    }
 	
 
 }
